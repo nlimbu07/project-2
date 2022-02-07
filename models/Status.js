0467c809e542bddf794f360405d45df40478d23b
Niraj Limbu <nlimbu@yahoo.com>
2022-02-07 12:04:12 -0500

    create status.js model

---
+++ models/Status.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Status extends Model {}
+
+Status.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      references: {
+        model: 'contact.status',
+        key: 'id',
+      },
+    },
+    status_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'status',
+  }
+);
+
+module.exports = Status;
