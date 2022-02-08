5c05198d85eae9e0b0717beda5fd7cf5e5095bdb
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-02-08 09:33:21 -0500

    Contact Data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,49 @@
 const router = require('express').Router();
+const { Contact, Lead_Source, Status, User } = require('../models');
+const sequelize = require('../config/connections');
+
+// router.get('/', (req, res) => {
+//   res.render('homepage', {
+//     id: 1,
+//     post_url: 'https://handlebarsjs.com/guide/',
+//     title: 'Handlebars Docs',
+//     created_at: new Date(),
+//     vote_count: 10,
+//     comments: [{}, {}],
+//     user: {
+//       username: 'test_user'
+//     }
+//   });
+// });
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Contact.findAll({
+    include: [
+        {
+        model: Status,
+        attributes: ['status_name']
+        },
+        {
+            model: Lead_Source,
+            attributes: ['lead_source_name']
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+}
+)
+  .then(dbContactData => res.json(dbContactData))
+    const contacts = dbContactData.map(contact => contact.get({plain:true}));
+    res.render('homepage', {
+      contacts
+      
+    })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connections');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
@@ -16,8 +21,4 @@ sequelize.sync({ force: false }).then(() => {
   });
 });
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
